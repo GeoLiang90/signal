8b72307b21a2c53c17f1056cd2f7e55dc31df349
George <gliang90@stuy.edu>
2018-11-08 23:12:39 -0500

    mostly done

--- signal.c
+++ signal.c
@@ -1,9 +1,25 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
 static void sighandler(int signo){
-  
+  if (signo == SIGUSR1){
+    printf("My PID: %d \n", getpid());
+  }
+  if (signo == SIGINT){
+    printf("Worked");
+
+    int crash = open("status.txt", O_WRONLY||O_RDONLY);
+    if (crash < 0){
+      perror("ERROR");
+      return;
+    }
+    int stats = write(crash, "You're program was terminated. \n",35);
+    exit(1);
+
+  }
 }
 
 int main(){
@@ -11,6 +27,7 @@ int main(){
     printf("I am currently running: %d \n", getpid());
     signal(SIGUSR1,sighandler);
     signal(SIGINT,sighandler);
+    kill(getpid(),SIGUSR1);
     sleep(1);
   }
   return 0;
